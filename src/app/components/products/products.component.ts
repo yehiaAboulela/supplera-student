8d211349a78716315890b7af8913addc8606fe1c
yehiaAboulela <yhia.themora@gmail.com>
2024-06-13 01:22:07 +0300

    first commit from mac

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  ngOnInit(): void {
+    Aos.init()
+  }
 }
